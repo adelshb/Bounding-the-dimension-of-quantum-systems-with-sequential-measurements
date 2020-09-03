904bcc561ab2a1ca9fd19b54c3768c0f6560b77b
adelshb <adel.sohbi@gmail.com>
2020-09-03 15:44:20 +0900

    Update random_witness_generator.py

--- script/random_witness_generator.py
+++ script/random_witness_generator.py
@@ -114,6 +114,16 @@ def NPA_bound(coef,X_basis):
     prob.solve(solver=cp.MOSEK, verbose=False)
     return prob.value
 
+    def str2bool(v):
+        if isinstance(v, bool):
+           return v
+        if v.lower() in ('yes', 'true', 't', 'y', '1'):
+            return True
+        elif v.lower() in ('no', 'false', 'f', 'n', '0'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError('Boolean value expected.')
+
 if __name__ == "__main__":
     parser = ArgumentParser()
 
